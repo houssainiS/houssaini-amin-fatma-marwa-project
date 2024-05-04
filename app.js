863e2ce974d7e7142d5fcc95cf2aa5526779cfd5
houssainiS <houssaini.hs@gmail.com>
2024-05-04 21:42:24 +0100

    created create-post.html page & made the route working

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve static files from the 'pages' folder
-app.use(express.static('pages'));
+app.use(express.static(path.join(__dirname, 'pages')));
 
 // Require route files
 const home = require('./routes/home');
 const createPost = require('./routes/createPost');
+
 // Use routes
 app.use('/', home);
-app.use('/create-post',createPost)
+app.use('/create-post', createPost); // Changed the route path to '/create-post'
 
 // Start server
 const PORT = process.env.PORT || 3000;
--- routes/createPost.js
+++ routes/createPost.js
@@ -1,11 +1,11 @@
-// routes/home.js
+// routes/createPost.js
 
 const express = require('express');
 const path = require('path');
 const router = express.Router();
 
 // Define routes
-router.get('/createPost', (req, res) => {
+router.get('/', (req, res) => { // Changed the route to '/'
   console.log('in createPost');
   res.sendFile(path.join(__dirname, '../pages/create-post.html'));
 });
